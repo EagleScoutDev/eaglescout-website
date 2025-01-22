7b217f2b324cd333bb3bd4244fc2095565e90cf4
Alan <alanlichen1@gmail.com>
2025-01-21 20:59:13 -0800

    Add footer to more info page, update spacing

--- app/get-started/page.tsx
+++ app/get-started/page.tsx
@@ -1,13 +1,15 @@
 import { DownloadSection } from '@/app/sections/DownloadSection';
 import { Navbar } from '@/app/components/Navbar';
 import { MoreInfo } from '@/app/get-started/components/MoreInfo';
+import { Footer } from '@/app/components/Footer';
 
 export default function GetStarted() {
     return (
         <div className="min-h-screen bg-black text-white">
-            <Navbar />
-            <DownloadSection />
-            <MoreInfo />
+            <Navbar/>
+            <DownloadSection/>
+            <MoreInfo/>
+            <Footer/>
         </div>
-    )
+    );
 }
