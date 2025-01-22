0f5c47f2cf3a83b3046ef38abe3bc58c8cb25257
Alan <alanlichen1@gmail.com>
2025-01-21 23:23:09 -0800

    Add animations at the consequence of no more server rendering. Fix later.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-import type { Metadata } from 'next';
+'use client';
 import './globals.css';
 import { titilliumWebRegular } from '@/lib/fonts';
 
-export const metadata: Metadata = {
-    title: 'EagleScout',
-    description: 'Next-generation scouting for FRC',
-};
+// export const metadata: Metadata = {
+//     title: 'EagleScout',
+//     description: 'Next-generation scouting for FRC',
+// };
 
 export default function RootLayout({
                                        children,
@@ -16,6 +16,24 @@ export default function RootLayout({
         <html lang="en">
         <body className={titilliumWebRegular.className}>
         {children}
+        <script dangerouslySetInnerHTML={{
+            __html: `
+            document.addEventListener('DOMContentLoaded', () => {
+                console.log('DOMContentLoaded');
+                setTimeout(() => {
+                    const elements = document.getElementsByClassName('animation-class');
+                    console.log(elements);
+                    for (let i = 0; i < elements.length; i++) {
+                        const element = elements[i];
+                        element.classList.remove('opacity-0');
+                        element.classList.remove('translate-y-12');
+                        element.classList.add('opacity-100');
+                        element.classList.add('translate-y-0');
+                    }
+                }, 100);
+            });
+            `,
+        }}/>
         </body>
         </html>
     );
